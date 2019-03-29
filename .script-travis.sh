b71512a4154c2b7efe80fb0d8c3c9941cd21e2fc
Michael Mestnik <cheako+github_com@mikemestnik.net>
2019-03-28 22:23:54 -0500

    Mike Mestnik(checko), for vkwayland

---
+++ .script-travis.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -ex
+
+case $FEATURES in
+    "all")
+        vagga cargo-"$TRAVIS_RUST_VERSION" test --all-features &&
+        vagga cargo-"$TRAVIS_RUST_VERSION" doc --no-deps --all-features
+    ;;
+    "default")
+        vagga cargo-"$TRAVIS_RUST_VERSION" test &&
+        vagga cargo-"$TRAVIS_RUST_VERSION" doc --no-deps
+    ;;
+    "examples")
+        vagga cargo-"$TRAVIS_RUST_VERSION" check --examples
+    ;;
+    "cargo-fmt")
+        vagga cargo-"$TRAVIS_RUST_VERSION" fmt --all -- --check
+    ;;
+    "cargo-clippy")
+        vagga cargo-"$TRAVIS_RUST_VERSION" clippy --all-features -- -D warnings
+    ;;
+    "anvil")
+        cd anvil
+        case $ANVIL_FEATURES in
+            "all")
+                vagga cargo-"$TRAVIS_RUST_VERSION" test --all-features
+            ;;
+            "default")
+                vagga cargo-"$TRAVIS_RUST_VERSION" test
+            ;;
+            *)
+                vagga cargo-"$TRAVIS_RUST_VERSION" check --no-default-features --features "$ANVIL_FEATURES"
+        esac
+    ;;
+    *)
+        vagga cargo-"$TRAVIS_RUST_VERSION" check --tests --no-default-features --features "$FEATURES" &&
+        vagga cargo-"$TRAVIS_RUST_VERSION" doc --no-deps --no-default-features --features "$FEATURES"
+esac
